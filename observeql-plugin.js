db27962f3661fd825eed958409d4046696af52a8
Jonathan Navarrete <jonatns@gmail.com>
2020-11-04 23:11:41 -0800

    Initial commit

---
+++ observeql-plugin.js
@@ -0,0 +1,63 @@
+import https from "https";
+import { printSchema } from "graphql";
+
+export default {
+  serverWillStart({ schema }) {
+    let body = JSON.stringify({
+      schema: printSchema(schema)
+    });
+
+    let options = {
+      hostname: "da75c4cc9006.ngrok.io",
+      path: "/api/schemas",
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Content-Length": Buffer.byteLength(body)
+      }
+    };
+
+    https
+      .request(options, (res) => {
+        let data = "";
+        res.on("data", (d) => {
+          data += d;
+        });
+        res.on("end", () => {
+          console.log(data);
+        });
+      })
+      .on("error", console.error)
+      .end(body);
+  },
+  requestDidStart({ request, schema }) {
+    if (request.operationName !== "IntrospectionQuery") {
+      let body = JSON.stringify({
+        query: request.query
+      });
+
+      let options = {
+        hostname: "da75c4cc9006.ngrok.io",
+        path: "/api/transactions",
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Content-Length": Buffer.byteLength(body)
+        }
+      };
+
+      https
+        .request(options, (res) => {
+          let data = "";
+          res.on("data", (d) => {
+            data += d;
+          });
+          res.on("end", () => {
+            console.log(data);
+          });
+        })
+        .on("error", console.error)
+        .end(body);
+    }
+  }
+};
---
+++ server.js
@@ -0,0 +1,34 @@
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { createServer } from "http";
+import cors from "cors";
+
+import schema from "./data/schema";
+import ObserveQLPlugin from "./observeql-plugin";
+
+const PORT = 8080;
+
+const app = express();
+
+app.use("*", cors());
+
+const server = new ApolloServer({
+  schema,
+  subscriptions: { path: "/websocket" },
+  plugins: [ObserveQLPlugin]
+});
+
+server.applyMiddleware({ app });
+
+const httpServer = createServer(app);
+
+server.installSubscriptionHandlers(httpServer);
+
+httpServer.listen(PORT, () => {
+  console.log(
+    `🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`
+  );
+  console.log(
+    `🚀 Subscriptions ready at ws://localhost:${PORT}${server.subscriptionsPath}`
+  );
+});
